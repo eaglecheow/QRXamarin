d62ee4b1fac347d05e39e4cc0bab345156a5473d
Eagle Cheow <eagle.cheow@hotmail.com>
2018-03-05 11:10:31 +0800

    Add project files.

---
+++ QRXamarin/QRXamarin.UWP/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace QRXamarin.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //Initialize QR Code Service
+            ZXing.Net.Mobile.Forms.WindowsUniversal.ZXingScannerViewRenderer.Init();
+            LoadApplication(new QRXamarin.App());
+        }
+    }
+}
---
+++ QRXamarin/QRXamarin/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+using ZXing.QrCode;
+
+namespace QRXamarin
+{
+	public partial class MainPage : ContentPage
+	{
+	    public QrCodeEncodingOptions QROptions { get; set; }
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private async void Button_OnClicked(object sender, EventArgs e)
+	    {
+	        var scannerPage = new ZXingScannerPage();
+	        await Navigation.PushAsync(scannerPage);
+
+	        scannerPage.OnScanResult += (result) =>
+	        {
+	            scannerPage.IsScanning = false;
+	            Device.BeginInvokeOnMainThread(async () =>
+	            {
+	                await Navigation.PopAsync();
+	                await DisplayAlert("Scanned Detail", result.Text, "OK");
+	            });
+	        };
+
+            QRCode.BarcodeOptions = new QrCodeEncodingOptions()
+            {
+                Height = 500,
+                Width = 500
+            };
+	    }
+
+	}
+}
